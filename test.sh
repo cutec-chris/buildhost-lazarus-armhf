3b7225365f43d452dc72b9519f7fc3592e96e415
chris <christian@ullihome.de>
2017-06-23 20:39:42 +0000

    'test.sh' löschen

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-string=$(docker logs mysql);
-
-if [[ ${string} == *"mysql  Ver 14.14 Distrib 5.5.52, for debian-linux-gnu (armv7l) using readline 6.3"* ]]; then
-  echo "Passed";
-  exit 0
-fi
-  echo "Failed"
-  exit 1
